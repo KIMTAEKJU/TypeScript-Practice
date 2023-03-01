47390cccf77a3b925f643efe528c9c6a1a4824ab
KIMTAEKJU <gkffntm12@naver.com>
2023-03-01 12:38:18 +0900

    feat: eslint 설정 진행중

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,40 @@
 module.exports = {
-    extends: [
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
         "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
     ],
-    plugins: [
-        "plugin:prettier/recommended"
-    ]
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
 }
